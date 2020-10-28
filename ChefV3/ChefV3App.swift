eec997b38c027dd119cedbdc57b388d3f77f1358
Max Klumker <maximilian.klumker@code.berlin>
2020-10-28 14:08:02 +0100

    Initial Commit

---
+++ ChefV3/ChefV3App.swift
@@ -0,0 +1,17 @@
+//
+//  ChefV3App.swift
+//  ChefV3
+//
+//  Created by Maximilian Klumker on 28.10.20.
+//
+
+import SwiftUI
+
+@main
+struct ChefV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
